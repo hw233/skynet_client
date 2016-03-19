2b508013878ce514261148d7e2d9ae89e68e30b1
sundream <linguanglianglgl@gmail.com>
2016-03-19 19:00:56 +0800

    fixbug

--- script/conf/srvlist.lua
+++ script/conf/srvlist.lua
@@ -1,14 +1,14 @@
 srvlist = {
 	gamesrv_100 = {
 		ip = "127.0.0.1",
-		port = 8000,
+		port = 8001,
 	},
 	gamesrv_101 = {
 		ip = "127.0.0.1",
-		port = 8001,
+		port = 8002,
 	},
 	out_gamesrv_100 = {
 		ip = "120.25.231.197",
-		port = 8000,
+		port = 8001,
 	}
 }
